f68b046f319439ca2f09e481950680055a64d130
Forhadul-Islam <forhadulislam@gmail.com>
2021-12-12 19:54:28 +0600

    Queue  implemented using c & c++

---
+++ Mizanur/DS/Queue/C/queue.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define SIZE 10
+
+typedef struct 
+{
+    int front;
+    int rear;
+    int data[SIZE];
+} Queue;
+
+//function protorype
+bool isFull(Queue *s);
+bool isEmpty(Queue *s);
+void enQueue(Queue *s, int item);
+int deQueue(Queue *s);
+void display(Queue *s);
+
+int main(void){
+    Queue my_queue;
+    my_queue.front = -1;
+    my_queue.rear = -1;
+
+    enQueue(&my_queue, 8);
+    enQueue(&my_queue, 55);
+    enQueue(&my_queue, 10);
+    enQueue(&my_queue, 26);
+    deQueue(&my_queue);
+    deQueue(&my_queue);
+    display(&my_queue);
+    display(&my_queue);
+    display(&my_queue);
+    deQueue(&my_queue);
+    isEmpty(&my_queue);
+    deQueue(&my_queue);
+    isEmpty(&my_queue);
+
+    return 0;
+}
+
+
+//QUEUE METHODS:
+//enQueue()
+//Dequeue()
+//isEmpty()
+//isFull()
+//Peek()
+
+bool isFull(Queue *s){
+    if(s->rear >= SIZE-1) return true;
+    else return false;
+}
+
+bool isEmpty(Queue *s){
+    if(s->front == -1){
+        printf("Queue is empty!\n");
+        return true;
+    }else{
+        printf("Queue is not empty!\n");
+        false;
+    }
+}
+
+void enQueue(Queue *s, int item){
+    if(isFull(s)){
+        printf("Queue is Full..\n");
+    }else{
+        if(isEmpty(s)){
+            s->front = 0;
+            s->rear +=1;
+            s->data[s->rear] = item;
+            printf("Inserted first element --> %d\n\n", item);
+        }else{
+            s->rear += 1;
+            s->data[s->rear] = item;
+            printf("Inserted --> %d\n\n", item);
+        }
+    }
+}
+
+
+int deQueue(Queue *s){
+    int item;
+    if(isEmpty(s)){
+        printf("Queue is empty. Status: ");
+        return -1;
+        printf("\n\n");
+    }else{
+        item = s->data[s->front];
+        s->front += 1;
+        printf("Deleted --> %d\n\n", item);
+        if(s->front > s->rear){
+            s->front = s->rear = -1;
+        }
+    }
+    return item;
+}
+
+
+void display(Queue *s){
+    if(isEmpty(s)){
+        printf("Nothing to display. Queue is empty!");
+    }else{
+        int item;
+        item = s->data[s->front];
+        printf("Top item --> %d\n\n", item);
+    }
+}
---
+++ Mizanur/DS/Queue/Cpp/queue.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#define QUEUE_SIZE 10
+
+using namespace std;
+
+class Queue {
+    private:
+    int data[QUEUE_SIZE], front, rear;
+
+    public:
+    Queue(){
+        front = rear = 0-1;
+    }
+
+    bool isEmpty(){
+        return front == -1 ? true : false;
+    }
+
+    bool isFull(){
+        return rear == QUEUE_SIZE - 1 && front == -1 ? true : false;
+    }
+
+    void enQueue(int item){
+        if(isFull()){
+            cout << "Queue is full";
+            return;
+        }else{
+            if(front == -1) front = 0;
+            ++rear;
+            data[rear] = item;
+            cout << "inserted -->" << item << endl;
+        }
+    }
+
+
+    int deQueue(){
+        int item;
+        if(isEmpty()){
+            cout << "Queue is empty!" << endl;
+            return -1;
+        }else{
+            item = data[front];
+            ++front;
+            if(front >= rear) {
+                front = rear = -1;
+            }
+        }
+
+        return item;
+    }
+
+    void peek(){
+        int item;
+        if(isEmpty()){
+             cout << "Queue is empty" << endl;
+             return;
+        }
+        item = data[front];
+        cout << endl
+            << "Front item -->" << item << endl;
+    }
+
+    void display(){
+        int i;
+        if(isEmpty()){
+             cout << endl << "Queue is empty!" << endl;
+             return;
+        }
+
+        cout << "Display:" << endl;
+        cout << endl << "FRONT = " << front;
+        for(i = front; i<= rear; ++i){
+            cout << endl << "Item = " << data[i] << endl;
+        }
+    }
+
+};
+
+
+int main(void){
+    Queue q;
+    q.peek();
+    q.isEmpty() ?  cout << "Queue is empty" << endl : cout << "Queue is not empty" << endl;
+    q.enQueue(5);
+    q.enQueue(6);
+    q.enQueue(7);
+    q.isEmpty() ? cout << "Queue is empty" : cout << "Queue is not empty";
+    q.peek();
+    q.deQueue();
+    q.peek();
+    q.enQueue(9);
+    q.enQueue(11);
+    q.enQueue(13);
+    q.enQueue(15);
+    q.display();
+}
